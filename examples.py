b0a477a33faf8a5a75f72e64ea950d2a5e02d741
Firemark <tiramo@o2.pl>
2013-05-23 00:36:49 +0200

    English fix

--- examples.py
+++ examples.py
@@ -9,7 +9,7 @@ from validator import *
 def validate_int():
     int_validator = Validator(int, {
         C >= 5: "Number is too small",
-        C != 8: "Number mustn't equals 8",
+        C != 8: "Number mustn't equal 8",
         C < 10:	"Number is too big",
         lambda x: True: "Number is always ok :-D"
     })
@@ -26,7 +26,7 @@ def validate_int():
 def validate_str():
     str_validator = Validator(str, {
         C.islower(): "String must be lower",
-        length(C) >= 4: "String is too small",
+        length(C) >= 4: "String is too short",
         C[-1].upper() == 'T': "Last char must be 'T'"
     })
 
@@ -42,9 +42,9 @@ def validate_str():
 def validate_date():
     date_validator = Validator(lambda s: datetime.strptime(s, "%d-%m-%Y"), (
         (lambda date: date.year <= datetime.now().year,
-            "Year mustn't be in future"),
-        (1 + C.day % 2, "Day must be parity"),
-        (C.month > 5, "Month must be older than May")
+            "Year mustn't be in the future"),
+        (1 + C.day % 2, "Day must be even"),
+        (C.month > 5, "Month must be later than May")
     ))
 
     date_data = [
