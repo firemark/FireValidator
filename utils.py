6b1607cdceb0ebf2415dae8930c60a31c9c9a5f1
Firemark <tiramo@o2.pl>
2013-05-24 23:52:11 +0200

    add band and bor

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 def length(c):
     return c.__len__()
 
+#Why Python? I cannot to override and and or method :(
+#bxxx - boolean xxx
+def band(a, b):
+    return a.__eval_str_arg__("({0} and {1})", b)
+
+def bor(a, b):
+    return b.__eval_str_arg__("({0} or {1})", b)
+
 def in_range(c, min, max):
-    return min <= c <= max
+    return band(min <= c, c <= max)
 
 def len_in_range(c, min, max):
-    return min <= c.__len__() <= max
+    cc = length(c)
+    return band(min <= cc, cc <= max)
--- validator.py
+++ validator.py
@@ -2,121 +2,120 @@ import types
 
 __all__ = ['C', 'Validator', 'ValidationError']
 
+
 class Condition(object):
 
     """Magic. Don't touch"""
 
-    __ps = None
-    __globals = {}
+    def __init__(self, parse_string="x", __globs__=None):
+        self.__ps__ = parse_string
+        self.__globs__ = __globs__ or {}
 
-    def __init__(self, parse_string="x", __globals=None):
-        self.__ps = parse_string
-        self.__globals = __globals or {}
-
-    def __eval_str_arg(self, string, *objs):
-        globs = self.__globals.copy()
+    def __eval_str_arg__(self, string, *objs):
+        globs = self.__globs__.copy()
         size = len(globs)
         names = []
         for i, obj in enumerate(objs):
-            name = "_%d" % (size + i)
-            globs[name] = obj
-            names.append(name)
+                name = "_%d" % (size + i)
+                globs[name] = obj
+                names.append(name)
 
-        return self.__class__(string.format(self.__ps, *names), globs)
+        #print(string.format(self.__ps__, *names), globs)
+
+        return self.__class__(string.format(self.__ps__, *names), globs)
 
     def __lt__(self, obj):
-        return self.__eval_str_arg("({0} < {1})", obj)
+        return self.__eval_str_arg__("({0} < {1})", obj)
 
     def __le__(self, obj):
-        return self.__eval_str_arg("({0} <= {1})", obj)
+        return self.__eval_str_arg__("({0} <= {1})", obj)
 
     def __gt__(self, obj):
-        return self.__eval_str_arg("({0} > {1})", obj)
+        return self.__eval_str_arg__("({0} > {1})", obj)
 
     def __ge__(self, obj):
-        return self.__eval_str_arg("({0} >= {1})", obj)
+        return self.__eval_str_arg__("({0} >= {1})", obj)
 
     def __eq__(self, obj):
-        return self.__eval_str_arg("({0} == {1})", obj)
+        return self.__eval_str_arg__("({0} == {1})", obj)
 
     def __ne__(self, obj):
-        return self.__eval_str_arg("({0} != {1})", obj)
+        return self.__eval_str_arg__("({0} != {1})", obj)
 
     def __or__(self, obj):
-        return self.__eval_str_arg("({0} or {1})", obj)
+        return self.__eval_str_arg__("({0} | {1})", obj)
 
     def __ror__(self, obj):
-        return self.__eval_str_arg("({1} or {0})", obj)
+        return self.__eval_str_arg__("({1} | {0})", obj)
 
     def __and__(self, obj):
-        return self.__eval_str_arg("({0} and {1})", obj)
+        return self.__eval_str_arg__("({0} & {1})", obj)
+
+    def __rand__(self, obj):
+        return self.__eval_str_arg__("({1} & {0})", obj)
 
     def __add__(self, obj):
-        return self.__eval_str_arg("({0} + {1})", obj)
+        return self.__eval_str_arg__("({0} + {1})", obj)
 
     def __radd__(self, obj):
-        return self.__eval_str_arg("({1} + {0})", obj)
+        return self.__eval_str_arg__("({1} + {0})", obj)
 
     def __sub__(self, obj):
-        return self.__eval_str_arg("({0} - {1})", obj)
+        return self.__eval_str_arg__("({0} - {1})", obj)
 
     def __rsub__(self, obj):
-        return self.__eval_str_arg("({1} - {0})", obj)
+        return self.__eval_str_arg__("({1} - {0})", obj)
 
     def __mul__(self, obj):
-        return self.__eval_str_arg("({0} * {1})", obj)
+        return self.__eval_str_arg__("({0} * {1})", obj)
 
     def __rmul__(self, obj):
-        return self.__eval_str_arg("({1} * {0})", obj)
+        return self.__eval_str_arg__("({1} * {0})", obj)
 
-    def __pow__(self, obj):
-        return self.__eval_str_arg("({0} ** {1})", obj)
+    def __pow__(self, obj, pow=None):
+        return (
+            self.__eval_str_arg__("({0} ** {1} % {2})", obj, pow) if pow
+            else self.__eval_str_arg__("({0} ** {1})", obj)
+        )
 
     def __rpow__(self, obj):
-        return self.__eval_str_arg("({1} ** {0})", obj)
-
-    def __nonzero__(self):
-        return self.__eval_str_arg("bool({0})")
+        return self.__eval_str_arg__("({1} ** {0})", obj)
 
     def __contains__(self, obj):
-        return self.__eval_str_arg("({1} in {0})", obj)
+        return self.__eval_str_arg__("({1} in {0})", obj)
 
     def __mod__(self, obj):
-        return self.__eval_str_arg("({0} % {1})", obj)
+        return self.__eval_str_arg__("({0} % {1})", obj)
 
     def __rmod__(self, obj):
-        return self.__eval_str_arg("({1} % {0})", obj)
+        return self.__eval_str_arg__("({1} % {0})", obj)
 
     def __getattr__(self, name):
-        return self.__class__("%s.%s" % (self.__ps, name), self.__globals.copy())
+        return self.__class__("%s.%s" % (self.__ps__, name), self.__globs__.copy())
 
     def __getitem__(self, key):
-        return self.__eval_str_arg("{0}[{1}]", key)
-
-    def __getslice__(self, i=None, j=None):
-        return self.__class__("%s[%s:%s]" % (self.__ps, i or "", j or ""),
-                              self.__globals)
+        return self.__eval_str_arg__("{0}[{1}]", key)
 
     def __call__(self, *args, **kwargs):
         if args:
             if kwargs:
-                return self.__eval_str_arg("{0}(*{1},**{2})", args, kwargs)
+                return self.__eval_str_arg__("{0}(*{1},**{2})", args, kwargs)
             else:
-                return self.__eval_str_arg("{0}(*{1})", args)
+                return self.__eval_str_arg__("{0}(*{1})", args)
         elif kwargs:
-            return self.__eval_str_arg("{0}(**{2})", kwargs)
+            return self.__eval_str_arg__("{0}(**{2})", kwargs)
         else:
-            return self.__eval_str_arg("{0}()")
+            return self.__eval_str_arg__("{0}()")
 
     def __hash__(self):
         return id(self)
 
     def __compile__(self):
         """compile eval string and return lambda to execute"""
-        globs = self.__globals.copy()
-        comp = compile(self.__ps, "", "eval")
+        globs = self.__globs__.copy()
+        comp = compile(self.__ps__, "", "eval")
         return lambda obj: bool(eval(comp, globs, {"x": obj}))
-                                     
+
 
 class Validator(object):
 
